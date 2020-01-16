7eaa7ee3765bf19ce7bd34558e95d0d9f81b39ff
github username <github email address>
2020-01-15 23:02:16 -0800

    created newuserform

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   namespace :api do
     namespace :v1 do
       resources :cards
@@ -9,6 +10,7 @@ end
 end
 
 # Rails.application.routes.draw do
+  resources :users
 #
 #   namespace :api do
 #     namespace :v1 do
